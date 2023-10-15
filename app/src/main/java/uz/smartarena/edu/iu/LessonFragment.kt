4b0e5a4213b806656a58d3178033f71a3c2117bf
Turg'unboyev Doniyorjon <developerdoni@gmail.com>
2023-10-15 14:48:03 +0500

    Release commits

--- app/src/main/java/uz/smartarena/edu/iu/LessonFragment.kt
+++ app/src/main/java/uz/smartarena/edu/iu/LessonFragment.kt
@@ -1,11 +1,16 @@
 package uz.smartarena.edu.iu
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import uz.smartarena.edu.databinding.FragmentLessonBinding
+
 
 class LessonFragment : Fragment() {
     private var _binding: FragmentLessonBinding? = null
@@ -16,6 +21,13 @@ class LessonFragment : Fragment() {
     ): View {
         _binding = FragmentLessonBinding.inflate(inflater, container, false)
         lifecycle.addObserver(binding.video)
+        binding.test.setOnClickListener {
+            findNavController().navigate(LessonFragmentDirections.actionLessonFragmentToTestFragment())
+        }
+        binding.tele.setOnClickListener {
+            val telegram = Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/+Fj-dzGROO6FlMjEy"))
+            startActivity(telegram)
+        }
         return binding.root
     }
 
--- app/src/main/java/uz/smartarena/edu/iu/ui/TestFragment.kt
+++ app/src/main/java/uz/smartarena/edu/iu/ui/TestFragment.kt
@@ -4,14 +4,79 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import uz.smartarena.edu.R
+import uz.smartarena.edu.data.local.TestLists
+import uz.smartarena.edu.databinding.FragmentTestBinding
 
 class TestFragment : Fragment() {
+    private var current = 1
+    private var rightCount = 0
+    private var data = TestLists.getTests()
+    private var check = false
+    private var _binding: FragmentTestBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var k: ArrayList<TextView>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_test, container, false)
+    ): View {
+        _binding = FragmentTestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        k = arrayListOf(binding.a, binding.b, binding.c, binding.d)
+        setVars()
+        k.forEach { click ->
+            click.setOnClickListener {
+                if (!check) {
+                    check = true
+                    click.setTextColor(resources.getColor(R.color.red))
+                    if (data.answer == click.text.toString()) {
+                        rightCount++
+                    }
+                    clickWrongAnswer()
+                } else {
+                    Toast.makeText(requireContext(), "Qayta tanlash mumkin emas", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        binding.enter.setOnClickListener {
+            k.forEach {
+                it.setTextColor(resources.getColor(R.color.seriy))
+            }
+            if (check) {
+                check = false
+                setVars()
+                current++
+                binding.textView3.setText("$current/10")
+                if (current == 11) {
+                    Toast.makeText(requireContext(), "Topilganlar : $rightCount", Toast.LENGTH_SHORT).show()
+                    findNavController().popBackStack()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Avval variantni tanlang", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun clickWrongAnswer() {
+        k.forEach { click ->
+            if (data.answer == click.text.toString()) {
+                click.setTextColor(resources.getColor(R.color.green))
+            }
+        }
+    }
+
+    fun setVars() {
+        data.variants.shuffle()
+        for (i in k.indices) {
+            k[i].setText(data.variants[i])
+        }
     }
 }
